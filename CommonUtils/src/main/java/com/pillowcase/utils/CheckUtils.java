afc12ea4db958dd529e8b2a610b11d8263c1b167
PillowCase <953812126@qq.com>
2020-09-12 17:53:37 +0800

    Normal Tools Add CheckUtils Code

---
+++ CommonUtils/src/main/java/com/pillowcase/utils/CheckUtils.java
@@ -0,0 +1,42 @@
+package com.pillowcase.utils;
+
+import java.util.regex.Pattern;
+
+/**
+ * Author      : PillowCase
+ * Create On   : 2020/9/10 17:03
+ * Description : 校验类
+ * 1.0.1版本提交
+ */
+public class CheckUtils {
+    /**
+     * 正则：手机号（精确）
+     * <p>移动：134(0-8)、135、136、137、138、139、147、150、151、152、157、158、159、178、182、183、184、187、188、198</p>
+     * <p>联通：130、131、132、145、155、156、166、171、175、176、185、186</p>
+     * <p>电信：133、153、173、177、180、181、189、199</p>
+     * <p>全球星：1349</p>
+     * <p>虚拟运营商：170</p>
+     */
+    public static final String REGEX_MOBILE_EXACT = "^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(16[6])|(17[0,1,3,5-8])|(18[0-9])|(19[8,9]))\\d{8}$";
+
+    /**
+     * 验证手机号（精确）
+     *
+     * @param input 待验证文本
+     * @return {@code true}: 匹配<br>{@code false}: 不匹配
+     */
+    public static boolean isMobileExact(final CharSequence input) {
+        return isMatch(REGEX_MOBILE_EXACT, input);
+    }
+
+    /**
+     * 判断是否匹配正则
+     *
+     * @param regex 正则表达式
+     * @param input 要匹配的字符串
+     * @return {@code true}: 匹配<br>{@code false}: 不匹配
+     */
+    public static boolean isMatch(final String regex, final CharSequence input) {
+        return input != null && input.length() > 0 && Pattern.matches(regex, input);
+    }
+}
